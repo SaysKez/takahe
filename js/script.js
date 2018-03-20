8d76c8a65c790ea836eca05f45c69ad29fa03445
Kezie Todd <kezle@ks-macbook-pro.home>
2018-03-20 15:28:26 +0000

    added css reset and cursor

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//CURSOR
+$('body').mouseover(function() {
+    $(this).css({
+      cursor: 'none'
+    });
+  });
+  
+  $(document).on('mousemove', function(e) {
+    $('#circle-hover').css({
+      left: e.pageX,
+      top: e.pageY
+    });
+    $('#circle').css({
+      left: e.pageX,
+      top: e.pageY
+    });
+  });
+  $('a').mouseover(function() {
+    $('#cursor').addClass('on-link');
+  })
+  $('a').mouseout(function() {
+    $('#cursor').removeClass('on-link');
+  })
